6de9122e3397d3a9b899b7501a1d4d4adcd1ef0e
kkwan0226 <kkwan0226@gmail.com>
2021-07-11 16:36:11 +0900

    solution template add

---
+++ programmers.co.kr/solution_template.py
@@ -0,0 +1,15 @@
+from timeit import default_timer as timer
+from datetime import timedelta
+
+def solution(A, K):
+    answer = ''
+    return answer
+
+if __name__=="__main__":
+    ## Test Case ###########
+    
+    ########################
+    start = timer()
+    print('answer: ', solution(A, K))
+    end = timer()
+    print('time: ', timedelta(seconds=end-start))
