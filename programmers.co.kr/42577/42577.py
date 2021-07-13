2a04f6efa77edcf13122f288c1d6f9a46b8f3204
kkwan0226 <kkwan0226@gmail.com>
2021-07-13 17:02:40 +0900

    42577 python solution

--- programmers.co.kr/42577/42577.py
+++ programmers.co.kr/42577/42577.py
@@ -1,13 +1,23 @@
 from timeit import default_timer as timer
 from datetime import timedelta
 
-def solution(pphone_book):
-    answer = True
-    return answer
+from collections import defaultdict
+
+def solution(phone_book):
+    #print(phone_book[2].startswith('12'))
+    count = defaultdict(int)
+    for idx, phone_num in enumerate(phone_book):
+        for idx_in in range(len(phone_num)):
+            count[phone_num[:idx_in+1]]+=1
+    
+    for idx in range(len(phone_book)):
+        if count[phone_book[idx]] > 1: return False
+    return True
 
 if __name__=="__main__":
     ## Test Case ############################################
-    phone_book = ["119", "97674223", "1195524421"]	
+    phone_book = ["12","123","1235","567","88"]
+    # ["119", "97674223", "1195524421"]	
     #########################################################
 
     start = timer()
