ad550c847d6569f27319346d46218871f80624f6
kkwan0226 <kkwan0226@gmail.com>
2021-07-16 14:24:58 +0900

    42579 python solution

--- programmers.co.kr/42579/42579.py
+++ programmers.co.kr/42579/42579.py
@@ -1,8 +1,32 @@
 from timeit import default_timer as timer
 from datetime import timedelta
 
+from collections import defaultdict
+from operator import itemgetter
+
 def solution(genres, plays):
-    answer = []
+    answer = list()
+
+    sum_plays = defaultdict(int)
+    for genre, play in zip(genres, plays):
+        sum_plays[genre] += play
+    sum_plays = sorted(sum_plays.items(), key=lambda x: x[1], reverse=True)
+
+    preprocessing = [[] for _ in range(len(sum_plays))]
+    idx = [i for i in range(len(genres))]
+    for idx, genre, play in zip(idx, genres, plays):
+        for count_play_idx, count_play in enumerate(sum_plays):
+            if count_play[0] == genre :
+                preprocessing[count_play_idx].append({'idx': idx, 'play': play})
+    
+    for order in range(len(sum_plays)):
+        preprocessing[order]= sorted(preprocessing[order], key=itemgetter('play'), reverse=True)
+        if len(preprocessing[order]) == 1:
+            answer.append(preprocessing[order][0]['idx'])
+        else:
+            answer.append(preprocessing[order][0]['idx'])
+            answer.append(preprocessing[order][1]['idx'])
+    
     return answer
 
 if __name__=="__main__":
